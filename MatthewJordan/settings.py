5f1c0fe165dc58e95ad0be86e549bf5acae66d68
verycumbersome <matthewsages@gmail.com>
2017-08-28 23:13:35 -0700

    Add base template

--- MatthewJordan/settings.py
+++ MatthewJordan/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'MatthewJordanApp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'MatthewJordan.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [''],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
