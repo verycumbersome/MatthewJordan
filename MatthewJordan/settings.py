9b72dae5c239ab9643bdb9a58bb5e96fad3016f7
MattyJ <Matthewsages@gmail.com>
2017-09-03 12:26:49 -0700

    Heroku (#1)
    * Add static rot
    
    * Add allowed host

--- MatthewJordan/settings.py
+++ MatthewJordan/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k3#jfag$e7hym#1t3bg-*ij^$1%v1ol##7rp9d4v70ha1ygfy9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'matthewjordan.herokuapp.com']
 
 
 # Application definition
@@ -120,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
