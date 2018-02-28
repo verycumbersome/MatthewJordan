122354fc7ce6e678471124faceac22b23361e291
verycumbersome <matthewsages@gmail.com>
2018-02-28 14:47:43 -0800

    Update works and switch to model based system for works

--- MatthewJordanApp/models.py
+++ MatthewJordanApp/models.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Work(models.Model):
+    title = models.CharField(max_length=50)
+    link = models.CharField(max_length=200)
+    content = models.TextField(max_length=2500)
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
--- MatthewJordanApp/views.py
+++ MatthewJordanApp/views.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Work
 
 def index(request):
-    return render(request, 'index.html')
+    works = Work.objects.order_by('created_date').reverse()
+    return render(request, 'index.html', {"works":works})
